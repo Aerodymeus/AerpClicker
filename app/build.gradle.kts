3e69dc39148d5bbdfa63becba5f0da5cfe30e55e
Aerodymeus <info@aerodymeus.de>
2025-08-29 22:50:13 +0200

    Update app version to 0.0.3
    This commit updates the application version code from 1 to 3 and the version name from "1.0" to "0.0.3" in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.aerodymeus.aerpclicker"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
